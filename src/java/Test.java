e9bc33de51b977bdbdd44ab6ebeeabb747959347
hsw <mafumafuma8@gmail.com>
2023-02-18 12:26:05 +0800

    init

---
+++ src/java/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+    public static void main(String[] args) {
+        String a = "bbb";
+        String bb = "ccc";
+        String[] split = bb.split("123");
+
+
+    }
+
+}
