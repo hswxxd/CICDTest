72abaead08ac2758dac171c2a326067cb91f34b8
hsw <mafumafuma8@gmail.com>
2023-04-02 17:52:08 +0800

    init

---
+++ src/main/java/com/hsw/spbtest/SpbtestApplication.java
@@ -0,0 +1,13 @@
+package com.hsw.spbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hsw/spbtest/SpbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsw.spbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
